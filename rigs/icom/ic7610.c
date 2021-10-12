PurpleBearSecurity/Hamlib

commit 28b7543de660c432b22d0035b250d32fb91797e4

Add stop_morse to Icom rigs https://github.com/Hamlib/Hamlib/issues/240

rigs/icom/ic7610.c
@@ -327,5 +327,5 @@ const struct rig_caps ic7610_caps =
     .get_split_vfo =  icom_get_split_vfo,
     .set_powerstat = icom_set_powerstat,
     .get_powerstat = icom_get_powerstat,
-    .send_morse = icom_send_morse
+    .stop_morse = icom_stop_morse
 };

rigs/icom/ic7100.c
@@ -391,5 +391,6 @@ const struct rig_caps ic7100_caps =
     .get_split_mode = icom_get_split_mode,
     .set_powerstat = icom_set_powerstat,
     .get_powerstat = icom_get_powerstat,
-    .send_morse = icom_send_morse
+    .send_morse = icom_send_morse,
+    .stop_morse = icom_stop_morse
 };

rigs/icom/ic7300.c
@@ -444,6 +444,7 @@ const struct rig_caps ic7300_caps =
     .power2mW = icom_power2mW,
     .mW2power = icom_mW2power,
     .send_morse = icom_send_morse,
+    .stop_morse = icom_stop_morse,
     .send_voice_mem = icom_send_voice_mem
 };
 
@@ -689,6 +690,7 @@ const struct rig_caps ic9700_caps =
     .power2mW = icom_power2mW,
     .mW2power = icom_mW2power,
     .send_morse = icom_send_morse,
+    .stop_morse = icom_stop_morse,
     .send_voice_mem = icom_send_voice_mem
 };
 
@@ -892,6 +894,7 @@ const struct rig_caps ic705_caps =
     .power2mW = icom_power2mW,
     .mW2power = icom_mW2power,
     .send_morse = icom_send_morse,
+    .stop_morse = icom_stop_morse,
     .send_voice_mem = icom_send_voice_mem
 };
 

rigs/icom/ic7410.c
@@ -273,5 +273,6 @@ const struct rig_caps ic7410_caps =
     .set_split_vfo =  icom_set_split_vfo,
     .get_split_vfo =  icom_mem_get_split_vfo,
     .send_morse =  icom_send_morse,
+    .stop_morse =  icom_stop_morse,
 
 };

rigs/icom/ic7700.c
@@ -312,5 +312,6 @@ const struct rig_caps ic7700_caps =
     .get_split_vfo =  icom_get_split_vfo,
     .set_powerstat = icom_set_powerstat,
     .get_powerstat = icom_get_powerstat,
-    .send_morse = icom_send_morse
+    .send_morse = icom_send_morse,
+    .stop_morse = icom_stop_morse
 };

rigs/icom/ic7800.c
@@ -315,7 +315,8 @@ const struct rig_caps ic7800_caps =
     .get_split_vfo =  icom_get_split_vfo,
     .set_powerstat = icom_set_powerstat,
     .get_powerstat = icom_get_powerstat,
-    .send_morse = icom_send_morse
+    .send_morse = icom_send_morse,
+    .stop_morse = icom_stop_morse
 };
 
 /*

rigs/icom/ic785x.c
@@ -321,7 +321,8 @@ const struct rig_caps ic785x_caps =
     .get_split_vfo =  icom_get_split_vfo,
     .set_powerstat =  icom_set_powerstat,
     .get_powerstat =  icom_get_powerstat,
-    .send_morse = icom_send_morse
+    .send_morse = icom_send_morse,
+    .stop_morse = icom_stop_morse
 
 };
 

rigs/icom/icom.c
@@ -6798,6 +6798,39 @@ int icom_send_morse(RIG *rig, vfo_t vfo, const char *msg)
     return RIG_OK;
 }
 
+/*
+ * icom_stop_morse
+ * Assumes rig!=NULL, msg!=NULL
+ */
+int icom_stop_morse(RIG *rig, vfo_t vfo)
+{
+    unsigned char ackbuf[MAXFRAMELEN];
+    unsigned char cmd[MAXFRAMELEN];
+    int ack_len = sizeof(ackbuf), retval;
+    int len;
+
+    rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
+
+    cmd[0] = 0xff;
+
+    retval = icom_transaction(rig, C_SND_CW, -1, (unsigned char *) cmd, 1,
+                              ackbuf, &ack_len);
+
+    if (retval != RIG_OK)
+    {
+        return retval;
+    }
+
+    if (ack_len != 1 || ackbuf[0] != ACK)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: ack NG (%#.2x), len=%d\n", __func__,
+                  ackbuf[0], ack_len);
+        return -RIG_ERJCTED;
+    }
+
+    return RIG_OK;
+}
+
 int icom_power2mW(RIG *rig, unsigned int *mwpower, float power, freq_t freq,
                   rmode_t mode)
 {

commit a4ce1157ab1e56fad3d27ac20e4066bdff8cac83

ic7610: Re-add send_morse
Commit 28b7543de660c432b22d0035b250d32fb91797e4 mistakenly removed
send_morse from ic7610, add it back.

rigs/icom/ic7610.c
@@ -475,6 +475,7 @@ const struct rig_caps ic7610_caps =
     .get_split_vfo =  icom_get_split_vfo,
     .set_powerstat = icom_set_powerstat,
     .get_powerstat = icom_get_powerstat,
+    .send_morse = icom_send_morse,
     .stop_morse = icom_stop_morse,
     .wait_morse = rig_wait_morse
 };